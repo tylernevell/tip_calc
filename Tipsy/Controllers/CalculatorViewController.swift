d2eff83bd4378cf5e53b26b8dbc24fcbc126d7d1
tylernevell <tylernevell@gmail.com>
2020-05-26 16:53:07 -0700

    Functionality to calculator view buttons added.

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
     
+    var tipAmount = 0.1
+    
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
@@ -18,15 +20,34 @@ class CalculatorViewController: UIViewController {
     
 
     @IBAction func tipChanged(_ sender: UIButton) {
-        zeroPctButton.isSelected = true
+        // highlight selected button and deselect the others
+        // when any button is selected, the following actions happen
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        sender.isSelected = true
+        
+        // grab the title of the button... can't believe I initially forgot how to do this
+        let buttonName = sender.currentTitle!
+        
+        // drop the % sign from the title and make it a double
+        let buttonValue = Double(String((buttonName.dropLast())))!
+        
+        // calculate the bugger
+        tipAmount = buttonValue / 100
     }
+    
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
     }
+    
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        // print our tip percentage
+        print(tipAmount)
+        print(splitNumberLabel.text!)
     }
-    
-    
 }
 
